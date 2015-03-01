c18d676a5b5edfd1767100874200d5f89db99368
Rory1994 <rs1019@york.ac.uk>
2015-03-01 20:08:06 +0000

    Fixed queries in project controller

--- controllers/projects.py
+++ controllers/projects.py
@@ -7,7 +7,7 @@ def create():
 def project():
 
     project_id = request.args(0)
-    project = database.get_open_project(project_id).first()
+    project = database.get_open_project(db, project_id).first()
 
     #If project doesnt exist or isn't open, redirect
     if project is None:
@@ -17,13 +17,13 @@ def project():
     if project.author_id == auth._get_user_id():
         response.flash = DIV("You own this project", _class="alert alert-info")
 
-    documents_for_project = database.get_project_open_documents(project.id)
-    data_fields_for_project = database.get_data_fields_for_project(project.id)
+    documents_for_project = database.get_project_open_documents(db, project.id)
+    data_fields_for_project = database.get_data_fields_for_project(db, project.id)
 
     #Retrieve documents that have already been transcribed by the user in this project. This
     #can be used to alert users to that fact on the page.
     documents_transcribed_by_user = database.get_documents_in_project_that_has_already_been_transcribed_by_user\
-                                    (project_id, auth._get_user_id())
+                                    (db, project_id, auth._get_user_id())
 
 
     return dict(project=project, documents_for_project=documents_for_project,
