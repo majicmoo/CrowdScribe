6dae7b2c6930bac4d0567a563bf2c0b2e29c899e
daphers <dka505@york.ac.uk>
2015-03-05 22:53:43 +0000

    Fix of fix of add_transcription
    Screw up of local repository meant I committed wrong file!

--- controllers/projects.py
+++ controllers/projects.py
@@ -29,3 +29,57 @@ def project():
     return dict(project=project, documents_for_project=documents_for_project,
                 data_fields_for_project=data_fields_for_project,
                 documents_transcribed_by_user = documents_transcribed_by_user)
+
+
+
+def add_transcription():
+
+    #Remove if project data not required in page
+    project_id = request.args(0)
+    project = database.get_open_project(db, project_id)
+
+    if project is None:
+        redirect(URL('default','index'))
+
+    document_id = request.args(1)
+    document = database.get_document(db, document_id)
+
+    if document is None:
+        redirect(URL('projects','project',args=[project_id]))
+    
+    form = FORM()
+
+    if project.author_id == auth._get_user_id():
+        response.flash = DIV("You own this project", _class="alert alert-info")
+    
+    elif auth._get_user_id is None:
+        response.flash = DIV("Please register to transcribe", _class="alert alert-info")
+        
+    elif not database.check_if_document_has_already_been_transcribed_by_user(db, document_id, auth._get_user_id):
+        response.flash = DIV("You have already transcribed this document", _class="alert alert-info")
+
+    elif document.status == 'Done':
+        response.flash = DIV("This document has already received the maximum number of transcriptions allowed", _class="alert alert-info")
+        
+    #Display transcription submission form if document image is open for transcriptions and
+    #user is authorised to make a submission (ie registered user, not project creator and has not already made
+    #transcription for document image)    
+    else:
+        #Create dynamic form according to number of data_fields
+        for data_field in database.get_data_fields_for_project(db, project_id):
+            label = '%s\n%s' % (data_field.name, data_field.short_description)
+            text_input = INPUT(_name=data_field.name)
+            form.append(SPAN(label, text_input))
+            
+        form.append(INPUT(_type='submit'))
+            
+        if form.process().accepts:
+            #Insert new transcription record to insert transcribed fields
+            transcription_id = db.transcription.insert(document_id=document_id, author_id=auth._get_user_id(), status='pending')
+            
+            #Inserts each transcribed field in db
+            for data_field in database.get_data_fields_for_project(db, project_id):
+                db.transcribed_field.insert(data_field_id=data_field.id, transcription_id=transcription_id, information=form.vars[data_field.name])
+
+    return dict(project=project, document=document, form=form)
+
