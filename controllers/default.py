f98002bcdcbc43e92e1fdf3067ed42c9e1797163
AliCormack <abgc500@york.ac.uk>
2015-04-07 14:43:51 +0100

    IMPORTANT BUG FIX : Fixed bug where having 0 projects would crash index.

--- controllers/default.py
+++ controllers/default.py
@@ -21,10 +21,12 @@ def index():
     # NOT REALLY LATEST PROJECTS
     latest_projects = database.get_open_projects()
 
-    # Featured Project
-    featured_project = latest_projects[1]
-    #print featured_project
-    featured_project_image = database.get_document_for_project_header(featured_project.id).image
+    #Featured Project
+    featured_project = None
+    featured_project_image = None
+    if latest_projects:
+        featured_project = latest_projects[1]
+        featured_project_image = database.get_document_for_project_header(featured_project.id).image
 
     return dict(latest_projects = latest_projects, database = database,
                 featured_project = featured_project, featured_project_image = featured_project_image)
