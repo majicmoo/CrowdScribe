d7d36089d609a1a9e9bf0b290fc441eec3a7b945
daphers <dka505@york.ac.uk>
2015-04-08 11:23:16 +0100

    Tidy up of search
    Allow searches of words as multiple strings
    Added comments

--- controllers/search.py
+++ controllers/search.py
@@ -6,17 +6,15 @@ def search_results():
 
     # Page Title
     response.title = "Search Results"    
-
-    print "--------------Begin-------------"
-    projects = database.get_open_projects()
-    #Lists for SELECT() helper in advanced search form
+    
+    # Lists for SELECT() helper in advanced search form
     tags = ["All", "Sport", "Theatre", "Military", "Journal Entries", "Architecture", "Citizen Information",
            "Religion", "Art", "Literature", "Finance", "Scientific", "Media", "Music", "Other"]
+    eras = ['BC','AD']
     orders = ['Alphabetical', 'Earliest', 'Latest']
     #['Most accepted transcriptions', 'Fewest accepted transcriptions','Nearest completion', 'Farthest from completion']
-    eras = ['BC','AD']
 
-    #Creates advanced search form
+    # Creates advanced search form
     advanced = FORM(
         INPUT(_name='advance', _id="advancetext"),
         LABEL("Category", SELECT(tags, _name='tag', requires=IS_IN_SET(tags))),
@@ -28,19 +26,12 @@ def search_results():
         LABEL("Sort by", SELECT(orders, _name='order', requires=IS_IN_SET(orders))),
         INPUT(_value='Refine search', _type='submit', _id="advancesubmit"), _method='GET'
     )
+    
+    # Start with all open projects in the database as results
+    projects = database.get_open_projects()
 
-    #Determine how to order results. If none specified (ie, new search), default
-    #order alphabetically
-    if request.vars.order == 'Earliest':
-        projects = database.get_open_projects().sort(lambda project: project.time_period_start_date)
-    elif request.vars.order == 'Latest':
-        projects = database.get_open_projects().sort(lambda project: project.time_period_end_date, reverse=True)
-    else:
-        projects = database.get_open_projects().sort(lambda project: project.name)
-
-    print request.vars
-
-    #Gets keywords
+    ###Filter by keyword###
+    # Gets keywords
     if request.vars.quicksearch is not None:
         searchstr = request.vars.quicksearch
         advanced.vars.advance = request.vars.quicksearch
@@ -50,53 +41,67 @@ def search_results():
     else:
         searchstr = None
 
-    print searchstr
-    #Filters by keywords (in name and description)
+    # Filter excludes results which do not include any keywords in either project title or description
     if searchstr is not None:
-        projects.exclude(lambda project: (searchstr.upper() not in project.name.upper())
-            and (searchstr.upper() not in project.description.upper()))
+        keywords = searchstr.split(' ')
+        projects.exclude(lambda project: search_project_for_keywords(keywords, project))
 
-    #Filters by tag
+    ###Filter by category###
     if (request.vars.category != 'All') and (request.vars.category is not None):
         projects.exclude(lambda project: str(request.vars.tag) != str(project.tag))
 
-    #Filters by date
-    if (request.vars.start_date is not None or request.vars.start_date == "") and (request.vars.start_date is None or request.vars.start_date != ""):
+    ###Filter by date###
+    if not empty_date_field(request.vars.start_date):
         start_date = convert_date_to_integer(request.vars.start_date, request.vars.start_era)
         advanced.vars.start_date = request.vars.start_date
         advanced.vars.start_era = request.vars.start_era
     else:
+        # Ensures all dates pass (if validation is fixed)
         start_date = -2016
 
-    if (request.vars.end_date is not None or request.vars.end_date == "") and (request.vars.end_date is None or request.vars.end_date != ""):
+    if not empty_date_field(request.vars.end_date):
         end_date = convert_date_to_integer(request.vars.end_date, request.vars.end_era)
         advanced.vars.end_date = request.vars.end_date
         advanced.vars.end_era = request.vars.end_era
     else:
+        # Ensures all dates pass (if validation is fixed)
         end_date = 2016
 
-    if request.vars.start_date is not None or request.vars.end_date is not None:
-        #Excludes unknown dates
+    if not empty_date_field(request.vars.start_date) or not empty_date_field(request.vars.end_date):
+        # Excludes unknown dates
         if request.vars.include_unknown_date != "on":
-            #exclude if end before start date or start after end date
+            # Exclude if end_date is before project's start date, start_date after project's end date or if project dates are none
             projects.exclude(lambda project: (project.time_period_start_date is None) or
                 (project.time_period_end_date < start_date) or (end_date < project.time_period_start_date))
 
-        #Includes unknown dates
+        # Includes unknown dates
         else:
-            #exclude if before start date or after end date or Date is none
+            # Exclude if end_date is before project's start date or start_date after project's end date
             projects.exclude(lambda project: (project.time_period_end_date < start_date) or
                 (end_date < project.time_period_start_date))
+            
+    ###Order results###
+    if request.vars.order == 'Earliest':
+        # Order by earliest start date
+        projects = projects.sort(lambda project: project.time_period_start_date)
+    elif request.vars.order == 'Latest':
+        # Order by latest end date
+        projects = projects.sort(lambda project: project.time_period_end_date, reverse=True)
+    else:
+        # Order alphabetically (default)
+        projects = projects.sort(lambda project: project.name)
 
-    #Have to clear request.vars before processing otherwise consecutive
-    #searches accumulate into lists rather than individual elements
-    #advanced.vars = request.vars
+    """
+    Have to clear request.vars before processing otherwise consecutive
+    searches accumulate into lists rather than individual elements
+    advanced.vars = request.vars
+    """
     request.vars = {}
-
+    
     #Does not validate
     if advanced.process(onvalidation=date_validator).accepted:
         #advanced.vars=request.vars
-        redirect(URL('browse', vars=advanced.vars))
+        redirect(URL('search', vars=advanced.vars))
     elif advanced.errors:
         response.flash='errors'
 
@@ -104,24 +109,53 @@ def search_results():
 
     return dict(advanced=advanced, projects=projects)
 
+
 def attach_header_image_to_projects(projects):
     for project in projects:
         project.header_image = database.get_document_for_project_header(project.id).image
 
     return projects
 
+
+def search_project_for_keywords(keywords, project):
+    exclude = True
+    
+    # Search project for keywords. If word is found, project is not excluded
+    for word in keywords:
+        if (word.upper() in project.name.upper()) or (word.upper() in project.description.upper()):
+            exclude = False
+            break
+        
+    return exclude
+
+
 def convert_date_to_integer(date, era):
     if era =="BC":
         return -int(date)
     else:
         return int(date)
+    
+def empty_date_field(date):
+    if (date is not None or date == "") and (date is None or date != ""):
+        #Date field has content
+        return False
+    else:
+        return True
 
 def date_validator(advanced):
-    #Check that start date is before end date
-    if ((request.vars.start_date is not None or request.vars.start_date == "") and (request.vars.start_date is None or request.vars.start_date != "") and
-        (request.vars.end_date is not None or request.vars.end_date == "") and (request.vars.end_date is None or request.vars.end_date != "")):
-
-        start = convert_date_to_integer(request.vars.start_date,request.vars.start_era)
-        end = convert_date_to_integer(request.vars.end_date,request.vars.end_era)
+    #Checks start date is after 2015 BC
+    if not empty_date_field(advanced.vars.start_date):
+        if convert_date_to_integer(advanced.vars.start_date) < -2015:
+            advanced.errors.start_date = 'Start date must be after 2015 BC'
+            
+    #Checks end date is before 2015 AD
+    if not empty_date_field(advanced.vars.end_date):
+        if convert_date_to_integer(advanced.vars.end_date) > 2015:
+            advanced.errors.end_date = 'End date must be before 2015 AD'        
+            
+    #Check that start date is before end date if there are entries for both dates
+    if not empty_date_field(advanced.vars.start_date) and not empty_date_field(advanced.vars.end_date):
+        start = convert_date_to_integer(advanced.vars.start_date, advanced.vars.start_era)
+        end = convert_date_to_integer(advanced.vars.end_date, advanced.vars.end_era)
         if start > end:
-            advanced.errors.time_period_end_date = 'The End Date of the time period must be later than the Start Date'
+            advanced.errors.end_date = 'The End Date of the time period must be later than the Start Date'
