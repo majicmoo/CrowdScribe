685724b0317a6bcbae9de22bf048cc6397dabe33
AliCormack <abgc500@york.ac.uk>
2015-04-12 19:48:21 +0100

    Started Browse page

--- controllers/browse.py
+++ controllers/browse.py
@@ -1,2 +1,15 @@
+import database_transactions as database_transactions
+database = database_transactions.DatabaseTransactions(db)
+from gluon import *
+
 def all():
-    return dict()
+
+    allprojects = attach_header_image_to_projects(database.get_open_projects())
+
+    return dict(allprojects = allprojects)
+
+def attach_header_image_to_projects(projects):
+    for project in projects:
+        project.header_image = database.get_document_for_project_header(project.id).image
+
+    return projects
