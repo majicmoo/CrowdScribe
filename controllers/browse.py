f879195576d3542e3987ffbab529889a55a62dd3
AliCormack <abgc500@york.ac.uk>
2015-04-14 18:06:05 +0100

    Revamped Project page with fields+better document display.

--- controllers/browse.py
+++ controllers/browse.py
@@ -6,7 +6,6 @@ from gluon import *
 
 def all():
 
-
     categories = ["Sport", "Theatre", "Military", "Journal Entries", "Architecture", "Citizen Information",
                "Religion", "Art", "Literature", "Finance", "Scientific", "Media", "Music", "Other"]
     allprojects = general_module.attach_all_information_to_projects(database.get_open_projects())
@@ -21,6 +20,10 @@ def category():
     categoryid = int(request.args(0))
     category = categories[categoryid]
     projects = database.get_projects_for_tag(category)
+
+    if not(projects):
+        session.flash = "No Projects currently available for "+category
+        redirect(URL('browse', 'all'))
 
     projects = general_module.attach_all_information_to_projects(projects)
 
